d75d9acb2e0f6f7cb5095046a7e6e80e82c8d41c
Renangoes03 <renangoessantos9@gmail.com>
2024-08-05 16:48:55 -0300

    Toolbar Menu Sair Finalizado

--- app/src/main/java/com/viajet/itubus/activity/activity/MainActivity.java
+++ app/src/main/java/com/viajet/itubus/activity/activity/MainActivity.java
@@ -1,26 +1,72 @@
 package com.viajet.itubus.activity.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.viajet.itubus.R;
+import com.viajet.itubus.activity.helper.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Configura toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Itubus");
+        setSupportActionBar(toolbar);
+
+        // Configurações de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_sair) {
+            deslogarUsuario();
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+        try {
+            autenticacao.signOut();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
